cfb881fb8afc9951ea331c2a25c517a9261c86f1
Muhammed Aashiq <aashiqmuhammed007@gmail.com>
2024-10-10 22:01:59 +0530

    Second Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)} >
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
