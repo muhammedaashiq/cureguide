e41c1aff9cb335c28ea99da7323b25d511e01a39
Muhammed Aashiq <aashiqmuhammed007@gmail.com>
2024-10-24 23:39:11 +0530

    Thursday

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -72,6 +72,15 @@ const PatientForm = () => {
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@email.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
        
         
 
