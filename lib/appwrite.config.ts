b87959101f76ab415fab0121406c904f74acd805
Muhammed Aashiq <aashiqmuhammed007@gmail.com>
2024-10-29 20:28:55 +0530

    appwrite 2

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,4 +1,4 @@
-import * as sdk form 'node-appwrite'
+import * as sdk from 'node-appwrite'
 
 const {
     PROJECT_ID,
@@ -12,3 +12,13 @@ const {
 } = process.env
 
 const client = new sdk.client()
+
+client
+    .setEndpoint(ENDPOINT!)
+    .setProject(PROJECT_ID)
+    .setKey(API_KEY)
+
+export const databases = new sdk.Databases(client);
+export const storage = new sdk.Storage(client);
+export const messaging = new sdk.Messaging(client);
+export const users = new sdk.Users(client);
